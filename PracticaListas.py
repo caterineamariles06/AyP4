c09586f9459da4fbbbb6a2e4a34b1188ff2d46ba
sala 105 <sala105@poliacademico.edu.loc>
2026-02-05 20:26:45 -0500

    Ejemplo de listas simples

---
+++ PracticaListas.py
@@ -0,0 +1,24 @@
+class Nodo:
+    def __init__(self, documento, nombre):
+        self.documento = documento
+        self.nombre = nombre
+        self.siguiente = None
+
+class Lista:
+    def __init__(self):
+        self.cabeza = None
+        self.cola= None
+
+    def AgregarAlFinal(self, documento, nombre):
+        nodo= Nodo(documento, nombre)
+
+        if self.cabeza == None:
+            self.cabeza= nodo
+            self.cola= nodo
+        else:
+            self.cola.siguiente = nodo
+            self.cola = nodo
+                   
+        
+        
+        
