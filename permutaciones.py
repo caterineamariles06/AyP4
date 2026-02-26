c94930a857ea95d0406f5431bbd52a4464d8c983
Cate <caterine_amariles82242@elpoli.edu.co>
2026-02-26 12:41:46 -0500

    Permutaciones ejemplo clase

---
+++ permutaciones.py
@@ -0,0 +1,16 @@
+def permutaciones(lista):
+    if len(lista) <=1:
+        return [lista]
+    resultado = []
+    for i in range(len(lista)):
+
+        elemento = lista[i]
+
+        resto = lista[:i] + lista[i+1:] # Lista sin el elemento actual
+
+        for perm in permutaciones(resto):
+            resultado.append([elemento] + perm)
+
+    return resultado
+
+print(permutaciones([1,2,7]))
